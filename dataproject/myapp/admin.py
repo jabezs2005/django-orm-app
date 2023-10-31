ed8aa2d34435d7580692b313a61933ce0502f26f
jabezs2005 <jabsudha2005@gmail.com>
2023-10-31 09:00:23 +0530

    first commit

---
+++ dataproject/myapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Student,StudentAdmin,Employee,EmployeeAdmin
+
+
+# Register your models here.
+admin.site.register(Student,StudentAdmin)
+admin.site.register(Employee,EmployeeAdmin)
+
+
